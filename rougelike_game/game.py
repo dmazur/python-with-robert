0a5301ccde8797158d9b528acf5271ad54de2095
Dawid Mazur <dmazur.work@gmail.com>
2019-08-09 22:32:20 +0200

    Added main loop and the ability to move a player

--- rougelike_game/game.py
+++ rougelike_game/game.py
@@ -1,23 +1,51 @@
+import subprocess as sp
+
 EMPTY_FIELD = '.'
 
-# nasza tablica
-level = []
+player_x = 3
+player_y = 3
 
-# wypełniamy pustą tablicę kropkami
-for x in range(20):
-    # stwórz wiersz z dwusdzietku pustych elementów listy
-    # i przypisz go do naszej tablicy
-    level.append([EMPTY_FIELD] * 20)
+def create_level():
+    # nasza tablica
+    level = []
+    # wypełniamy pustą tablicę kropkami
+    for x in range(20):
+        # stwórz wiersz z dwusdzietku pustych elementów listy
+        # i przypisz go do naszej tablicy
+        level.append([EMPTY_FIELD] * 20)
+    return level
 
-level[2][5] = '@'
+def draw_level(level):
+    # drukowanie zawartości tablicy level
+    for x in range(20):
+        for y in range(20):
+            # wydrukuj element tablicy o współrzędnych x,y
+            # normalnie instrukcja print przechodzi do nowej linii
+            # (nowa linia to specjalny niewidoczny znak '\n')
+            print(level[y][x], end='')
+        # tutaj na każdym końcu wyższej pętli drukujemy pusty znak
+        # a print automatycznie przechodzi jeszcze do nowej linii
+        print('')
 
-# drukowanie zawartości tablicy level
-for x in range(20):
-    for y in range(20):
-        # wydrukuj element tablicy o współrzędnych x,y
-        # normalnie instrukcja print przechodzi do nowej linii
-        # (nowa linia to specjalny niewidoczny znak '\n')
-        print(level[y][x], end='')
-    # tutaj na każdym końcu wyższej pętli drukujemy pusty znak
-    # a print automatycznie przechodzi jeszcze do nowej linii
-    print('')
+
+while True:
+    command = input('> Please, write your command: ')
+    sp.call('clear',shell=True)
+
+    if command == 'exit':
+        print('Goodbye!')
+        break
+
+    print("You've written " + command)
+
+    if command == 'up': player_y -= 1
+    elif command == 'down': player_y += 1
+    elif command == 'right': player_x += 1
+    elif command == 'left': player_x -= 1
+    else: print('ERROR: Wrong command')
+
+    level = create_level()
+
+    level[player_x][player_y] = '@'
+
+    draw_level(level)
