d5fd024479c05aed25008af3f74a05c3fa2db7bf
Dawid Mazur <dmazur.work@gmail.com>
2019-09-12 20:04:40 +0200

    Lesson printing

--- lesson_schedule/lesson_schedule.py
+++ lesson_schedule/lesson_schedule.py
@@ -4,3 +4,8 @@ class LessonSchedule:
 
     def addLesson(self, lesson):
         self.lessons.append(lesson)
+
+    def print_all(self):
+        for lesson in self.lessons:
+            print(lesson.day_of_week + ' ' + str(lesson.hour) + ':' + str(lesson.minute) + ' ' + lesson.name)
+
--- lesson_schedule/main.py
+++ lesson_schedule/main.py
@@ -2,6 +2,14 @@ from lesson import Lesson
 from lesson_schedule import LessonSchedule
 
 lesson1 = Lesson('Monday', 8, 0, 'Polish')
+lesson2 = Lesson('Monday', 8, 50, 'English')
+lesson3 = Lesson('Tuesday', 9, 45, 'Math')
+lesson4 = Lesson('Monday', 8, 0, 'Polish')
 
 lesson_schedule = LessonSchedule()
 lesson_schedule.addLesson(lesson1)
+lesson_schedule.addLesson(lesson2)
+lesson_schedule.addLesson(lesson3)
+lesson_schedule.addLesson(lesson4)
+
+lesson_schedule.print_all()
